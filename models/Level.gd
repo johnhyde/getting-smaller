cbca354c1e55da2fcbdde086aab30d3482ee75a5
John Hyde <johnhydefour@gmail.com>
2021-03-26 22:21:59 -0700

    adding outline shader, though it is bad

--- models/Level.gd
+++ models/Level.gd
@@ -5,6 +5,8 @@ class_name Level
 export(Array, Resource) var child_scenes = []
 var child_instances = []
 var i_got_it = false;
+var static_body
+var basic_with_outline_mat = preload("res://basic.tres")
 #onready var starting_pos = translation / scale
 #onready var trimesh = create_trimesh_collision()
 
@@ -12,6 +14,10 @@ var i_got_it = false;
 # Called when the node enters the scene tree for the first time.
 func _ready():
   add_to_group('levels')
+  static_body = find_node('static_collision')
+#  var material = static_body.get_parent().get_surface_material(0)
+  var material = static_body.get_parent().set_surface_material(0, basic_with_outline_mat)
+#  material.next_pass = basic_with_outline_mat
   var timer = Timer.new()
   timer.connect('timeout', self, '_all_grown_up')
   timer.set_wait_time( .1 )
